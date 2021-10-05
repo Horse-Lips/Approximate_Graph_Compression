83a4d631ec20dcbf0015385fc68a991b05d6cdc4
AFeatureNotaBug <callmep@outlook.com>
2021-10-05 19:44:44 +0100

    Added to Vertex class
    - Added basic fields and getters/setters

--- GraphCompression/src/graphComponents/Vertex.java
+++ GraphCompression/src/graphComponents/Vertex.java
@@ -1,5 +1,56 @@
 package graphComponents;
 
-public class Vertex {
+import java.util.List;
 
+
+/*Vertex class used for representing Graph nodes*/
+public class Vertex {
+	private int index;		//Index of the Vertex in the Graph Vertex list
+	private List<Integer> adjList;	//List of Vertices adjacent to the Vertex
+	private int value;		//Value stored in the Vertex
+	private Vertex parent;	//Parent of Vertex (e.g. in shortest path)
+	
+	public Vertex(int index) {
+		this.index = index;
+		
+	}
+	
+	
+	/*Returns the index of the Vertex in the Graph Vertex list*/
+	public int getIndex() {
+		return this.index;
+	}
+	
+	
+	/*Returns the adjacency list of the Vertex*/
+	public List<Integer> getAdj() {
+		return this.adjList;
+	}
+	
+	/*Adds a vertex to the adjacency list*/
+	public void addToAdj(int v) {
+		this.adjList.add(v);
+	}
+	
+	
+	/*Returns the value of the Vertex*/
+	public int getVal() {
+		return this.value;
+	}
+	
+	/*Updates the value of the Vertex*/
+	public void setVal(int value) {
+		this.value = value;
+	}
+	
+	
+	/*Returns the parent Vertex of the current Vertex*/
+	public Vertex getParent() {
+		return this.parent;
+	}
+	
+	/*Updates the parent of the Vertex*/
+	public void setParent(Vertex parent) {
+		this.parent = parent;
+	}
 }
