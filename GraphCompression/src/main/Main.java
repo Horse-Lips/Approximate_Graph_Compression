e0f573e028e048c096e824a4b7d9bdcfcfe78281
AFeatureNotaBug <callmep@outlook.com>
2021-10-17 22:34:04 +0100

    Created exampleGraph2.txt, larger than the other one

--- GraphCompression/src/main/Main.java
+++ GraphCompression/src/main/Main.java
@@ -8,7 +8,7 @@ import graphUtils.General;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		String file = "C:/Users/CallM/Documents/Life/UniGlasgow/Year4/Project/Code/exampleGraph.txt";
+		String file = "C:/Users/CallM/Documents/Life/UniGlasgow/Year4/Project/Code/exampleGraph2.txt";
 		
 		Graph G1 = General.fromFile(file);
 		Graph G2 = General.fromFile(file);
@@ -16,18 +16,18 @@ public class Main {
 		long startTime, endTime;
 		
 		startTime = System.nanoTime();
-		System.out.println(G1);
 		G1.Gauss(0);
-		System.out.println(G1);
 		G1.Gauss(0);
-		System.out.println(G1);
 		G1.Gauss(0);
-		System.out.println(G1);
+		G1.Gauss(0);
+		G1.Gauss(0);
 		endTime = System.nanoTime();
 		
 		System.out.println("Gaussian time: " + (endTime - startTime));
 		
 		startTime = System.nanoTime();
+		G2.contract(0);
+		G2.contract(0);
 		G2.contract(0);
 		G2.contract(0);
 		G2.contract(0);
